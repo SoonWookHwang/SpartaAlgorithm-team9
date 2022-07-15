83817c5ac06338be402b3578fe0c81ce37f55ec6
awg333 <awg333@naver.com>
2022-07-15 16:49:04 +0900

    문제 11, 15, 19 해결

--- src/Algo19.java
+++ src/Algo19.java
@@ -1,2 +1,23 @@
-package src;public class Algo19 {
+package src;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Algo19 {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings);
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if(s1.charAt(n) > s2.charAt(n)) return 1;
+                else if(s1.charAt(n) == s2.charAt(n)){
+                    return 0;
+                }
+                else return -1;
+            }
+        });
+
+
+        return strings;
+    }
 }
