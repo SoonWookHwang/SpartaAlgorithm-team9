bd19524a608d4257b381884e37eaf9187230a9e1
Elrendar <guuto9@gmail.com>
2022-07-15 14:51:58 +0900

    알고리즘4 해결

---
+++ src/Algo4.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+public class Algo4 {
+    public static String Solution(String[] participant, String[] completion) {
+        String answer = "";
+        var counter = new HashMap<String, Integer>();
+        
+        for (String s : participant) {
+            if (!counter.containsKey(s))
+                counter.put(s, 1);
+            else
+                counter.put(s, counter.get(s) + 1);
+        }
+        for (String s : completion) {
+            if (!counter.containsKey(s))
+                counter.put(s, 1);
+            else
+                counter.put(s, counter.get(s) - 1);
+        }
+
+        for (var s : counter.keySet()) {
+            if (counter.get(s) > 0) {
+                answer = s;
+            }
+        }
+
+        return answer;
+    }
+}
