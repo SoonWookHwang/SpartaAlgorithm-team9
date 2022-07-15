bef14ae22bc8e88ecc4f4975899cf1bdf23470e5
awg333 <awg333@naver.com>
2022-07-15 14:37:18 +0900

    문제 3번

--- Algo03.java
+++ Algo03.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Algo3 {
+public class Algo03 {
+    public String solution(int n) {
+        String answer = "";
+        char[] subak = {'수', '박'};
+        for(int i = 0; i < n; i++){
+            answer += subak[i%2];
+        }
+        return answer;
+    }
 }
