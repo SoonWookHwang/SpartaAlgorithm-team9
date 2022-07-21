8d688508aa23bdd91bfd8a93c25c13aa5a72c78b
Elrendar <guuto9@gmail.com>
2022-07-21 13:34:24 +0900

    .gitignore 수정, Algo38 클래스명 중복 수정

--- src/Algo38.java
+++ src/Algo38.java
@@ -1,15 +1,16 @@
-import java.util.*;
+import java.util.ArrayList;
 
-class Solution {//프로그래머스 크레인 인형뽑기 게임  stack
-    public int solution(int[][] board, int[] moves) {
+class Algo38 {//프로그래머스 크레인 인형뽑기 게임  stack
+
+    public static int solution(int[][] board, int[] moves) {
         ArrayList<Integer> aList = new ArrayList<Integer>();
         // [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[0,2,4,4,2],[3,5,1,3,1]]
         // [1,5,3,5,1,2,1,4]
-        for (int i = 0; i < moves.length; i++){
+        for (int i = 0; i < moves.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                if (board[j][moves[i] -1] != 0) {
-                    aList.add(board[j][moves[i] -1]);
-                    board[j][moves[i] -1] = 0;
+                if (board[j][moves[i] - 1] != 0) {
+                    aList.add(board[j][moves[i] - 1]);
+                    board[j][moves[i] - 1] = 0;
                     break;
                 }
             }
@@ -17,14 +18,14 @@ class Solution {//프로그래머스 크레인 인형뽑기 게임  stack
 
         int answer = 0;
         for (int i = 0; i < aList.size() - 1; i++) {//aList = 4 2
-            if (aList.get(i) == aList.get(i+1)) {
+            if (aList.get(i) == aList.get(i + 1)) {
                 aList.remove(i);
                 aList.remove(i);
                 answer += 2;
                 i = -1;
             }
         }
-        
+
         return answer;
     }
 } 
